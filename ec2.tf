91e1f6045012ae56154f7e3d1380f4ed8f2d3dcf
Eric Rodrigues <69876912+eric1014@users.noreply.github.com>
2021-06-16 19:08:35 -0300

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "apache_webserver" {
   ami                    = data.aws_ami.ubuntu_latest.id
   instance_type          = var.instance_type
-  iam_instance_profile   = aws_iam_instance_profile.ec2_s3_iam_profile.name
   user_data              = file("scripts/install_apache.sh")
   subnet_id              = aws_subnet.private_ec2_a.id
   vpc_security_group_ids = [aws_security_group.sg_ec2_apache.id]
