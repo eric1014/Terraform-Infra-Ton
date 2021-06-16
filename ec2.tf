e3a862c1fad3a881b381d869a8291efa4cac5737
Eric Rodrigues <69876912+eric1014@users.noreply.github.com>
2021-06-16 18:39:38 -0300

    Update ec2.tf
    Adjusting variables and adding count

--- ec2.tf
+++ ec2.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "apache_webserver" {
   user_data              = file("scripts/install_apache.sh")
   subnet_id              = aws_subnet.private_ec2_a.id
   vpc_security_group_ids = [aws_security_group.sg_ec2_apache.id]
+  count                  = var.count_instances
 
   tags = {
     Name        = "${var.name}-${var.env}"
